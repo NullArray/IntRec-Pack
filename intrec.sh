ad537e6576b822b9f1e1aa3877f124f7332499f2
NullArray <NullArray@users.noreply.github.com>
2017-09-15 22:10:57 +0200

    Removed unnecesary debug logging.
    While testing i had some parts of the shell script log certain operations for debugging purposes. The test version had an `mkdir` command in order to create a directory to save those logs. Since the release version did not require this kind of logging to take place i had removed the command responsible for creating the log directory, however i forgot to remove the commands that appended information to a file that now(according to the script) was supposed to go into a directory that did not exist. Which resulted in an error. That's since been fixed.

--- intrec.sh
+++ intrec.sh
@@ -119,12 +119,12 @@ function get_gdriver()
 		notification "x32 architecture detected..."
 		sleep 1
       
-		wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux32.tar.gz | tee -a $cwd/IntRec-Logs/gdriver_install.log
-		tar -xvf geckodriver-v0.18.0-linux32.tar.gz | tee -a $cwd/IntRec-Logs/gdriver_install.log
-		rm geckodriver-v0.18.0-linux32.tar.gz | tee -a $cwd/IntRec-Logs/gdriver_install.log
-		chmod +x geckodriver | tee -a $cwd/IntRec-Logs/gdriver_install.log
-		mv geckodriver /usr/sbin | tee -a $cwd/IntRec-Logs/gdriver_install.log
-		sudo ln -s /usr/sbin/geckodriver /usr/bin/geckodriver | tee -a $cwd/IntRec-Logs/gdriver_install.log
+		wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux32.tar.gz
+		tar -xvf geckodriver-v0.18.0-linux32.tar.gz
+		rm geckodriver-v0.18.0-linux32.tar.gz 
+		chmod +x geckodriver 
+		mv geckodriver /usr/sbin 
+		sudo ln -s /usr/sbin/geckodriver /usr/bin/geckodriver
       
 		notification "Geckodriver has been succesfully installed."
 	fi
@@ -301,7 +301,7 @@ function list()
 					
 					sudo pip install argparse dnspython requests
 					
-					notification "Sublist3r was nstalled succesfully."
+					notification "Sublist3r was succesfully installed."
 				fi
 				tools
 				printf "%b \n"
